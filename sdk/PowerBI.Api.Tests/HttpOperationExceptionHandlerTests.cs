47c9cde9e5e1b5325123bfcd6b41222f3674ea71
Michael Blake <mikeblake@hotmail.com>
2018-07-03 13:14:28 +0100

    Add HttpOperationExceptionHandler

---
+++ sdk/PowerBI.Api.Tests/HttpOperationExceptionHandlerTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.PowerBI.Api.V2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.PowerBI.Api.Extensions.V2;
+using Microsoft.Rest;
+
+namespace PowerBI.Api.Tests
+{
+    [TestClass]
+    public class HttpOperationExceptionHandlerTests
+    {
+        private const string AccessKey = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+        [TestMethod]
+        public async Task NoPowerBIHttpOperationExceptionHandler()
+        {
+            var importResponse = CreateSampleImportResponse();
+
+            using (var handler = new FakeHttpClientHandler(importResponse))
+            using (var client = CreatePowerBIClient(handler))
+            using (var stream = new MemoryStream())
+            {
+                try
+                {
+
+                    await client.Imports.PostImportWithFileAsync(stream, "name");
+                }
+                catch (Exception exception)
+                {
+                    Assert.AreEqual("Operation returned an invalid status code 'Forbidden'", exception.Message);
+                }
+            }
+        }
+
+        [TestMethod]
+        public async Task PowerBIHttpOperationExceptionHandler()
+        {
+            var importResponse = CreateSampleImportResponse();
+
+            using (var handler = new FakeHttpClientHandler(importResponse))
+            using (var client = CreatePowerBIClient(handler, new HttpOperationExceptionHandler()))
+            using (var stream = new MemoryStream())
+            {
+                try
+                {
+
+                    await client.Imports.PostImportWithFileAsync(stream, "name");
+                }
+                catch (Exception exception)
+                {
+                    Assert.AreEqual("Power BI operation returned an error code 'ADGraphGroupOperationFailedResourceNotFound'", exception.Message);
+                }
+            }
+        }
+
+        // {"error":{"code":"InvalidRequest","message":"datasetId is null or empty"}}
+        // {"error":{"code":"GeneralException","message":"Failed to process dataset PostDataset","target":"PostDataset","details":[{"message":"SQL operation failed: The Database operation reached a timeout of '00:01:30'"}]}}
+        // {"error":{"code":"DM_GWPipeline_UnknownError","pbi.error":{"code":"DM_GWPipeline_UnknownError","parameters":{},"details":[]}}}
+        // {"error":{"code":"FeatureNotAvailableError","pbi.error":{"code":"FeatureNotAvailableError","parameters":{},"details":[]}}}
+        // {"error":{"code":"UnknownError","pbi.error":{"code":"UnknownError","parameters":{},"details":[],"exceptionCulprit":1}}}
+        // {"error":{"code":"ResourceLimitsPackageCountExceeded","pbi.error":{"code":"ResourceLimitsPackageCountExceeded","parameters":{"CountPermitted":"200"},"details":[]}}}
+        // {"error":{"code":"ADGraphGroupOperationFailedResourceNotFound","pbi.error":{"code":"ADGraphGroupOperationFailedResourceNotFound","parameters":{},"details":[],"exceptionCulprit":1}}}
+        private static HttpResponseMessage CreateSampleImportResponse()
+        {
+            return new HttpResponseMessage(HttpStatusCode.Forbidden)
+            {
+                Content = new StringContent(@"{""error"":{""code"":""ADGraphGroupOperationFailedResourceNotFound"",""pbi.error"":{""code"":""ADGraphGroupOperationFailedResourceNotFound"",""parameters"":{},""details"":[],""exceptionCulprit"":1}}}")
+            };
+        }
+
+        private IPowerBIClient CreatePowerBIClient(HttpClientHandler handler, params DelegatingHandler[] handlers)
+        {
+            var credentials = new TokenCredentials(AccessKey);
+            return new PowerBIClient(credentials, handler, handlers);
+        }
+    }
+}
---
+++ sdk/PowerBI.Api/Extensions/V2/HttpOperationExceptionHandler.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Rest;
+using Microsoft.Rest.Serialization;
+using Newtonsoft.Json;
+
+namespace Microsoft.PowerBI.Api.Extensions.V2
+{
+    /// <inheritdoc />
+    public class HttpOperationExceptionHandler : DelegatingHandler
+    {
+        const string XPowerBIErrorDetailsHeaderKey = "X-PowerBI-Error-Details";
+        const string XPowerBIErrorInfoHeaderKey = "X-PowerBI-Error-Info";
+        const string RequestIdHeaderKey = "RequestId";
+
+        /// <inheritdoc />
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = await base.SendAsync(request, cancellationToken);
+            if (response.IsSuccessStatusCode)
+            {
+                return response;
+            }
+
+            string responseContent = null;
+            if (response.Content != null)
+            {
+                responseContent = response.Content.ReadAsStringAsync().Result;
+            }
+            var message = $"Power BI operation returned an invalid status code '{response.StatusCode}'";
+
+            var errorBody = ErrorInHeader(response, XPowerBIErrorDetailsHeaderKey);
+            var errorInfo = ErrorInHeader(response, XPowerBIErrorInfoHeaderKey);
+            errorBody = errorBody ?? responseContent;
+
+            var errorTypeSafe = ConvertBodyToPowerBIError(errorBody);
+            if (!string.IsNullOrEmpty(errorTypeSafe?.Message))
+            {
+                message = errorTypeSafe.Message;
+            }
+            else if (!string.IsNullOrEmpty(errorTypeSafe?.Code))
+            {
+                message = $"Power BI operation returned an error code '{errorTypeSafe.Code}'";
+            }
+
+            if (!string.IsNullOrEmpty(errorInfo))
+            {
+                message += $" ({errorInfo})";
+            }
+
+            var ex = new PowerBIOperationException(message)
+            {
+                Code = errorTypeSafe?.Code ?? errorInfo,
+                Body = errorBody,
+                Request = new HttpRequestMessageWrapper(request, null),
+                Response = new HttpResponseMessageWrapper(response, responseContent)
+            };
+
+            if (response.Headers.Contains(RequestIdHeaderKey))
+            {
+                ex.RequestId = response.Headers.GetValues(RequestIdHeaderKey).FirstOrDefault();
+            }
+
+            response.Dispose();
+            throw ex;
+        }
+
+        static string ErrorInHeader(HttpResponseMessage response, string key)
+        {
+            if (response.Headers == null)
+            {
+                return null;
+            }
+
+            IEnumerable<string> errorHeaderValues;
+            if (response.Headers.TryGetValues(key, out errorHeaderValues))
+            {
+                var xHeaderErrorDetails = errorHeaderValues?.FirstOrDefault();
+                if (xHeaderErrorDetails != null)
+                {
+                    return xHeaderErrorDetails;
+                }
+            }
+            return null;
+        }
+
+        static PowerBIError ConvertBodyToPowerBIError(string body)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(body))
+                {
+                    var json = SafeJsonConvert.DeserializeObject<PowerBIErrorBody>(body);
+                    return json?.Error;
+                }
+            }
+            catch (JsonException)
+            {
+            }
+            return null;
+        }
+
+        public class PowerBIOperationException : HttpOperationException
+        {
+            public PowerBIOperationException(string message)
+                : base(message)
+            {
+            }
+
+            public string RequestId { get; set; }
+
+            public string Code { get; set; }
+        }
+
+        public class PowerBIErrorBody
+        {
+            [JsonProperty(PropertyName = "error")]
+            public PowerBIError Error { get; set; }
+        }
+
+        public class PowerBIError
+        {
+            [JsonProperty(PropertyName = "code")]
+            public string Code { get; set; }
+
+            [JsonProperty(PropertyName = "details")]
+            public IEnumerable<PowerBIExceptionDetails> Details { get; set; }
+
+            [JsonProperty(PropertyName = "message")]
+            public string Message { get; set; }
+
+            [JsonProperty(PropertyName = "target")]
+            public string Target { get; set; }
+        }
+
+        public class PowerBIExceptionDetails
+        {
+            [JsonProperty(PropertyName = "message")]
+            public string Message { get; set; }
+        }
+    }
+}
